6ac524533aeeae1b29353aa6eb4d976db10e44a9
Anas TULIMAT <anas.tulimat@orange.com>
2025-05-16 14:44:31 +0200

    feat: add dashboard admin template

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/dashboard')]
+final class DashboardController extends AbstractController
+{
+
+    #[Route(name: 'app_dashboard', methods: ['GET'])]
+    public function index(): Response
+    {
+        // Liste des entités disponibles pour la navigation
+        $entities = [
+            ['name' => 'Users', 'route' => 'app_user_index'],
+            ['name' => 'Products', 'route' => 'app_product_index'],
+            ['name' => 'Orders', 'route' => 'app_order_index'],
+        ];
+
+        return $this->render('admin/dashboard/index.html.twig', [
+            'entities' => $entities,
+        ]);
+    }
+}
