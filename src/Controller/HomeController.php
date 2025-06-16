2317542c0104a37dd101ad8f85d9bb35a8837e2d
Anas TULIMAT <anas.tulimat@orange.com>
2025-06-16 14:51:44 +0200

    fix: display images by position on the home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Image;
 use App\Repository\GalleryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,9 +16,10 @@ final class HomeController extends AbstractController
         $homeGallery = $galleryRepository->findOneBy(['slug' => 'home']);
         $images = $homeGallery->getImages();
 
+        /** @var Image[] $sortedImages */
         $sortedImages = $images->toArray();
-        usort($sortedImages, function($a, $b) {
-            return strcmp($a->getId(), $b->getId());
+        usort($sortedImages, function(Image $a, Image $b) {
+            return strcmp($a->getPosition(), $b->getPosition());
         });
 
         return $this->render('home/index.html.twig', [
